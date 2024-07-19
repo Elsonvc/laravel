964450b54dd963219136194c09f15cb5be2f5634
Elsonvc <145981191+Elsonvc@users.noreply.github.com>
2024-07-19 14:46:16 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,5 +2,14 @@
 sudo chown -R ec2-user:ec2-user /home/ec2-user/laravel
 cd /home/ec2-user/laravel
 composer install --no-progress --prefer-dist --optimize-autoloader
+# Install Node.js and npm
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+
+# Install PHP dependencies
+composer install --no-progress --prefer-dist --optimize-autoloader
+
+# Install Node.js dependencies
 npm install
+
 
