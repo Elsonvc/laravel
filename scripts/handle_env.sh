ead926918b8779b53817d29fe3c0a7bdd8f94447
Elsonvc <145981191+Elsonvc@users.noreply.github.com>
2024-07-23 11:53:22 +0530

    Update handle_env.sh

--- scripts/handle_env.sh
+++ scripts/handle_env.sh
@@ -3,7 +3,8 @@
 # Retrieve the parameter value
 LARAVEL_ENV=$(aws ssm get-parameter --name "laravel_env" --query "Parameter.Value" --output text)
 
+# Create the target directory if it does not exist
+mkdir -p /var/www/laravel
+
 # Create or update the .env file with the parameter value
 echo "LARAVEL_ENV=$LARAVEL_ENV" > /var/www/laravel/.env
-
-# You can add other environment variables similarly if needed
