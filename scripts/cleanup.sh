18e78b50635c8d730c6c42d7a6f4effe187bf1f6
Elsonvc <145981191+Elsonvc@users.noreply.github.com>
2024-07-23 11:00:04 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Remove the conflicting file
 rm -f /home/ec2-user/laravel/phpunit.xml
+rm -f /home/ec2-user/laravel/.env.example
+
 
 # Optionally, you can add more cleanup commands if needed
 # e.g., remove directories, backup files, etc.
